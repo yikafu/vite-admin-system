eb7f82182d57a14371ff13a4def3937d4e9297bf
yikafu <f2149804524@gmail.com>
2024-01-25 16:04:18 +0800

    创建header组件

---
+++ src/components/PHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="dropdown">
+    <el-dropdown>
+      <span class="el-dropdown-link">
+          <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>个人中心</el-dropdown-item>
+          <el-dropdown-item>退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.dropdown{
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+.el-dropdown-link:focus-visible{
+  outline: none;
+}
+</style>

2081ae287d516db43362f4194472882df8c4be73
yikafu <f2149804524@gmail.com>
2024-02-05 15:28:39 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  // @路径配置
+  resolve: {
+    alias: {
+      "@": "/src",
+      public: "/public",
+      views: "/src/views",
+    },
+  },
+});

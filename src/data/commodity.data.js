322f823c89f0c9df748130404df468b35beeee98
yikafu <f2149804524@gmail.com>
2024-02-05 15:29:16 +0800

    数据源修改 使结构更清晰

---
+++ src/data/commodity.data.js
@@ -0,0 +1,82 @@
+const commodityData = [
+  {
+    id: 19182,
+    name: "iPhone 14 Pro Max",
+    price: 10000,
+    stock: 1000,
+  },
+  {
+    id: 21283,
+    name: "小米12 Pro",
+    price: 8000,
+    stock: 800,
+  },
+  {
+    id: 32391,
+    name: "华为P50 Pro",
+    price: 6000,
+    stock: 600,
+  },
+  {
+    id: 42304,
+    name: "iPad Pro 2024",
+    price: 5000,
+    stock: 500,
+  },
+  {
+    id: 53335,
+    name: "MacBook Pro 2024",
+    price: 4000,
+    stock: 400,
+  },
+  {
+    id: 61512,
+    name: "AirPods Pro 2",
+    price: 3000,
+    stock: 300,
+  },
+  {
+    id: 73552,
+    name: "Apple Watch Series 8",
+    price: 2000,
+    stock: 200,
+  },
+  {
+    id: 81357,
+    name: "iPhone 14",
+    price: 1000,
+    stock: 100,
+  },
+  {
+    id: 92134,
+    name: "小米12",
+    price: 800,
+    stock: 80,
+  },
+  {
+    id: 10111,
+    name: "华为P50",
+    price: 600,
+    stock: 60,
+  },
+  {
+    id: 11246,
+    name: "iPad Air 2024",
+    price: 500,
+    stock: 50,
+  },
+  {
+    id: 12109,
+    name: "MacBook Air 2024",
+    price: 400,
+    stock: 40,
+  },
+  {
+    id: 12436,
+    name: "AirPods 3",
+    price: 300,
+    stock: 30,
+  },
+];
+
+export default commodityData;

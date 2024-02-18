4f31133ad8e7f694b167ea7ae2b12ba670335cfb
yikafu <f2149804524@gmail.com>
2024-02-18 14:32:56 +0800

    commit

--- src/data/user.data.js
+++ src/data/user.data.js
@@ -1,7 +1,7 @@
 const userData = {
   username: "John Doe",
   role: "超级管理员",
-  avatar: "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
+  avatar: "https://www.gravatar.com/avatar",
   email: "12345678@gmail.com",
   phone: "1234567890",
   password: "123456",
